9885414580dbdcee4c7df05222dde2c8612493b8
pengfeng <pengfeng@rayootech.com>
2018-09-17 23:56:42 +0800

    demo

--- src/main/java/com/pengfeng/demo/Demo.java
+++ src/main/java/com/pengfeng/demo/Demo.java
@@ -1,7 +1,18 @@
 package com.pengfeng.demo;
 
 public class Demo {
-    public static void main(String[] args) {
-        System.out.println("haha!");
-    }
+
+	private static final String str = "0123456789abcdefghjklmnpqrstuvwxy";
+
+	public static void main(String[] args) {
+		System.out.println(getCode());
+	}
+	public static String getCode() {
+		StringBuilder code = new StringBuilder();
+		for (int i = 0, length = str.length(); i < 8; i++)
+			code.append(str.charAt((int) (Math.random() * length)));
+
+		return code.toString();
+
+	}
 }
